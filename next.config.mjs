4c39624818c6b0e7bb79c0d19d6bf40f5f02801b
Tia Senjaya <tiasenjaya@gmail.com>
2025-10-31 23:59:59 +0700

    build: remove static export (enable Node runtime & SSR)

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // JANGAN pakai: output: 'export'
-  // biarkan default agar API Routes & Prisma jalan di server (Node.js)
-};
+const nextConfig = {}
 
-export default nextConfig;
+export default nextConfig
